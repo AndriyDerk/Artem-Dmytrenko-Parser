d6e016df2de974d96328f2a00c4f8fb6207ccff8
AndriyDerk <AndriyDerk@gmail.com>
2023-11-04 20:50:24 +0200

    start

--- routers/index.js
+++ routers/index.js
@@ -4,6 +4,7 @@ const parserRouter = require('./parser.router')
 
 
 router.use('/parser', parserRouter)
-
-
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 module.exports = router
