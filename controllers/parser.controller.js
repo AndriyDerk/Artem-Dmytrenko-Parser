7707a1d1c808a4abee6105e754c6123cb8d801be
AndriyDerk <AndriyDerk@gmail.com>
2023-11-06 00:41:51 +0200

    logs

--- controllers/parser.controller.js
+++ controllers/parser.controller.js
@@ -5,14 +5,14 @@ class parserController{
         try{
             let first_name, second_name, email, street, apartment_number, city, phone_number, post_code
 
-            first_name = req.body.form_fileds[name]
-            second_name = req.body.form_fields[field_7e04b27]
-            email = req.body.form_fields[email]
-            street = req.body.form_fields[field_2ee6ead]
-            apartment_number = req.body.form_fields[field_6fd6def]
-            city = req.body.form_fields[field_89dc639]
-            phone_number = req.body.form_fields[field_3dceee2]
-            post_code = req.body.form_fields[field_4ec2f55]
+            first_name = req.body['form_fileds[name]']
+            second_name = req.body['form_fields[field_7e04b27]']
+            email = req.body['form_fields[email]']
+            street = req.body['form_fields[field_2ee6ead]']
+            apartment_number = req.body['form_fields[field_6fd6def]']
+            city = req.body['form_fields[field_89dc639]']
+            phone_number = req.body['form_fields[field_3dceee2]']
+            post_code = req.body['form_fields[field_4ec2f55]']
 
 
             console.log({first_name, second_name, email, street, apartment_number, city, phone_number, post_code})
