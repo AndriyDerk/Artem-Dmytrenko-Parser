9b811462ee8585e16877c752deda15eb2e22a144
AndriyDerk <AndriyDerk@gmail.com>
2023-11-05 20:58:48 +0200

    added logs

--- routers/index.js
+++ routers/index.js
@@ -5,6 +5,7 @@ const parserRouter = require('./parser.router')
 
 router.use('/parser', parserRouter)
 router.get('/', (req, res) => {
+    console.log("Ok")
     res.send('Artem-Dmytrenko-Parser parser')
 })
 module.exports = router
